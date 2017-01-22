1f8b6fd1f25ae43431b562d391c54d803114a9d5
codekraft-studio <info@codekraft.it>
2017-01-22 21:08:54 +0100

    created parent generator

---
+++ utils/generator.js
@@ -0,0 +1,78 @@
+var banner = require('./banner.js');
+// var mkdirp = require('mkdirp');
+var Generator = require('yeoman-generator');
+
+var gruntConfig = function(generator) {
+
+  generator.fs.copyTpl(
+    generator.templatePath('grunt/_package.json'),
+    generator.destinationPath('package.json'),
+    generator.props
+  );
+
+  generator.fs.copy(
+    generator.templatePath('grunt/_Gruntfile.js'),
+    generator.destinationPath('Gruntfile.js')
+  );
+
+};
+
+var gulpConfig = function(generator) {
+
+  generator.fs.copyTpl(
+    generator.templatePath('gulp/_package.json'),
+    generator.destinationPath('package.json'),
+    generator.props
+  );
+
+  generator.fs.copy(
+    generator.templatePath('gulp/_gulpfile.js'),
+    generator.destinationPath('gulpfile.js')
+  );
+
+};
+
+module.exports = Generator.extend({
+
+  constructor: function (args, opts) {
+    Generator.apply(this, arguments);
+    this.argument('appname', { type: String, required: false, description: 'The project folder name.' });
+  },
+
+  prompting: function () {
+    // Show the banner
+    this.log(banner);
+
+    // Get the questions
+    var prompts = require('./prompts.js')(this);
+
+    // Run it
+    return this.prompt(prompts).then(function (props) {
+      this.props = props;
+    }.bind(this));
+  },
+
+  configuring: function() {
+
+    switch (this.props.projectManager) {
+      case 'grunt':
+        gruntConfig(this);
+        break;
+      case 'gulp':
+        gulpConfig(this);
+        break;
+    }
+
+  },
+
+  install: function () {
+    this.installDependencies({
+      bower: false
+    });
+  },
+
+  end: function() {
+    this.log('\nYour project is ready, everything was installed successfully. Ciao!');
+  }
+
+});
