bb367289ee1deb5f64d28b4ff172096c37c8640f
Filippo Conti <filippo@codekraft.it>
2017-11-03 12:26:41 +0100

    Added template prompt close #5

--- utils/prompts.js
+++ utils/prompts.js
@@ -1,19 +1,57 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 const _ = require('lodash');
+
+const defaultProjectManager = 'grunt';
+const projectManagers = [{
+  name: 'Nothing',
+  value: ''
+}, {
+  name: 'Grunt',
+  value: 'grunt'
+}, {
+  name: 'Gulp',
+  value: 'gulp'
+}];
+
+const getTemplates = function () {
+  let dir = path.join(os.homedir(), '.wordpress-starter');
+  if (!fs.existsSync(dir)) {
+    return;
+  }
+  return fs.readdirSync(dir);
+};
 
 const validateRequired = function (value) {
   if (value === '') {
-    return 'This field is required, please enter a valid value.';
+    return 'This field is required, please enter a valid value';
   }
   return true;
 };
+
+// Export default settings
+module.exports.defaultProjectManager = defaultProjectManager;
+module.exports.projectManagers = projectManagers;
 
 // Function that validate for required input
 module.exports.validateRequired = validateRequired;
 
 // The prompts array
 module.exports.prompt = function (base) {
+  // Option to choose default template
+  let defaultTemplate = {
+    name: 'Default',
+    value: ''
+  };
+
+  // Try to get any custom existing template
+  // in the generator appdata folder on user home directory
+  let customTemplates = getTemplates();
+  let templateChoices = customTemplates ? [defaultTemplate].concat(customTemplates) : [defaultTemplate];
+
   return [
     {
       type: 'text',
@@ -22,7 +60,7 @@ module.exports.prompt = function (base) {
       default: _.kebabCase(base.appname),
       validate: function (input) {
         if (!/^(?:[a-z0-9]+-?[a-z0-9]+)+$/g.test(input)) {
-          return 'You should follow the WordPress plugin name standard.';
+          return 'You should follow the WordPress plugin name standard';
         }
         return true;
       }
@@ -40,31 +78,45 @@ module.exports.prompt = function (base) {
       name: 'projectDescription',
       message: 'What is the project description?',
       default: function (answers) {
-        return 'This is the ' + answers.projectTitle + ' description.';
+        return 'This is the ' + answers.projectTitle + ' description';
       }
     },
     {
-      type: 'text',
-      name: 'projectManager',
-      message: 'Do you want to use grunt or gulp as your build system? (Leave blank if you dont want to use anything)',
+      type: 'list',
+      name: 'projectTemplate',
+      message: 'Which template do you want to use?',
       default: function () {
-        return base.options.template ? '' : 'grunt';
-      },
-      validate: function (input) {
-        if (['', 'grunt', 'gulp'].indexOf(input) === -1) {
-          return 'You must use grunt, gulp or leave it blank.';
+        let template = base.options.template;
+        if (!template) {
+          return '';
         }
-        return true;
+        let index = templateChoices.indexOf(template);
+        return (index > -1) ? template : '';
+      },
+      choices: templateChoices,
+      // Show this prompt only if any custom template exists
+      // otherwise it will be useless and probably cause bugs
+      when: function () {
+        return customTemplates && customTemplates.length;
+      }
+    },
+    {
+      type: 'list',
+      name: 'projectManager',
+      message: 'What do you want to use as project manager?',
+      choices: projectManagers,
+      default: function () {
+        return (base.options.template) ? 0 : defaultProjectManager;
       }
     },
     {
       type: 'text',
       name: 'projectVersion',
-      message: 'The version to initialize this project.',
+      message: 'The version to initialize this project',
       default: '0.0.1',
       validate: function (input) {
         if (!/^\bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)$/.test(input)) {
-          return 'You should enter a valid version.';
+          return 'You should enter a valid semver version';
         }
         return true;
       }
